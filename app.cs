f53cf062516b40d12e606cad4434848e6da1a593
maajithadev <majithadev@gmail.com>
2024-03-21 12:50:48 +0530

    Cloning learning

--- app.cs
+++ app.cs
@@ -3,5 +3,6 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("This is my new Version for Cloud_Practice branch");
+        Console.WriteLine("Trial :) ");
     }
 }
