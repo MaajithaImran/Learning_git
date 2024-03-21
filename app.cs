3e381c7233c7e73cbedcbbb43df8f0bc84c41b4d
maajithadev <majithadev@gmail.com>
2024-03-21 11:37:20 +0530

    Add app.cs to Cloud_practice branch

--- app.cs
+++ app.cs
@@ -1,1 +1,7 @@
-Console.WriteLine("This is my third Version");
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("This is my new Version for Cloud_Practice branch");
+    }
+}
